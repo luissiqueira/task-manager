a30c17328cf6e2c8a463bec1bc4f9fe86a0ce785
luissiqueira <luissiqueirajr@gmail.com>
2019-11-12 00:29:12 -0300

    new: new models

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,50 @@
 from django.contrib import admin
-from .models import Project, Task
+from django.contrib.contenttypes.admin import GenericStackedInline
+
+from .models import Project, Task, Organization, ObjectRole, ObjectAttachment, TaskNote
+
+
+class ObjectRoleInlineAdmin(GenericStackedInline):
+    model = ObjectRole
+    extra = 0
+    fields = ('user', 'role',)
+
+
+class ObjectAttachmentInlineAdmin(GenericStackedInline):
+    model = ObjectAttachment
+    extra = 0
+    fields = ('attachment',)
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    readonly_fields = ('deleted',)
+    inlines = [ObjectRoleInlineAdmin, ObjectAttachmentInlineAdmin]
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for obj in instances:
+            if not obj.id and hasattr(obj, 'creator'):
+                obj.creator = request.user
+        formset.save()
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status')
-    list_filter = ('status',)
-    search_fields = ('title', 'description')
+    list_display = ('title', 'status', 'organization')
+    list_filter = ('status', 'organization')
+    search_fields = ('title', 'description', 'organization__name')
+    readonly_fields = ('deleted',)
+    inlines = [ObjectRoleInlineAdmin, ObjectAttachmentInlineAdmin]
+
+
+class TaskNoteInlineAdmin(admin.StackedInline):
+    model = TaskNote
+    extra = 0
+    exclude = ('deleted', 'creator')
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ('deleted',)
+    inlines = [ObjectAttachmentInlineAdmin, TaskNoteInlineAdmin]
--- core/models.py
+++ core/models.py
@@ -1,7 +1,45 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
-class Project(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField('Data de criação', auto_now_add=True)
+    updated_at = models.DateTimeField('Data de modificação', auto_now=True)
+    deleted = models.BooleanField('Deletado?', default=False)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Criador',
+                                null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Role(object):
+    ROLE_ADMIN = 'admin'
+    ROLE_OPERATIONAL = 'operational'
+    ROLE_VIEWER = 'viewer'
+
+    ROLES_CHOICES = (
+        (ROLE_ADMIN, 'Administrador'),
+        (ROLE_OPERATIONAL, 'Operador'),
+        (ROLE_VIEWER, 'Visualizador'),
+    )
+
+
+class Organization(BaseModel):
+    name = models.CharField('Nome', max_length=120)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Organização'
+        verbose_name_plural = 'Organizações'
+        ordering = ('name',)
+
+
+class Project(BaseModel):
     STATUS_OPEN = 'open'
     STATUS_CLOSED = 'closed'
 
@@ -10,6 +48,8 @@ class Project(models.Model):
         (STATUS_CLOSED, 'Finalizado')
     )
 
+    organization = models.ForeignKey(Organization, verbose_name='Organização', on_delete=models.CASCADE,
+                                     related_name='projects', null=True, blank=True)
     title = models.CharField('Título', max_length=120)
     description = models.TextField('Descrição', blank=True)
     status = models.CharField('Status', max_length=80, choices=STATUS_CHOICES, default=STATUS_OPEN)
@@ -23,7 +63,41 @@ class Project(models.Model):
         ordering = ('title',)
 
 
-class Task(models.Model):
+class ObjectRole(BaseModel):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    object = GenericForeignKey('content_type', 'object_id')
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Usuário',
+                             related_name='roles')
+    role = models.CharField('Papel', choices=Role.ROLES_CHOICES, default=Role.ROLE_VIEWER, max_length=80)
+
+    def __str__(self):
+        return f'{self.object} - {self.user} - {self.role}'
+
+    class Meta:
+        verbose_name = 'Papel'
+        verbose_name_plural = 'Papéis'
+        ordering = ('created_at',)
+
+
+class ObjectAttachment(BaseModel):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    object = GenericForeignKey('content_type', 'object_id')
+
+    attachment = models.FileField('Anexo')
+
+    def __str__(self):
+        return f'{self.object} - {self.attachment.name}'
+
+    class Meta:
+        verbose_name = 'Anexo'
+        verbose_name_plural = 'Anexos'
+        ordering = ('created_at',)
+
+
+class Task(BaseModel):
     STATUS_PENDING = 'pending'
     STATUS_DOING = 'doing'
     STATUS_DONE = 'done'
@@ -46,9 +120,6 @@ class Task(models.Model):
         (PRIORITY_HIGH, 'Alta')
     )
 
-    created_at = models.DateTimeField('Data de criação', auto_now_add=True)
-    updated_at = models.DateTimeField('Data de modificação', auto_now=True)
-
     project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name='Projeto', related_name='tasks')
     title = models.CharField('Título', max_length=120)
     description = models.TextField('Descrição', blank=True)
@@ -65,6 +136,21 @@ class Task(models.Model):
         verbose_name = 'Entrega'
         verbose_name_plural = 'Entregas'
         ordering = ('agreed_date', 'created_at')
+
+
+class TaskNote(BaseModel):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, verbose_name='Entrega', related_name='notes')
+    title = models.CharField('Título', max_length=120)
+    content = models.TextField('Anotação')
+    resolved = models.BooleanField('Resolvido?', default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Nota'
+        verbose_name_plural = 'Notas'
+        ordering = ('created_at',)
 
     # participantes do time
     # stakeholders
